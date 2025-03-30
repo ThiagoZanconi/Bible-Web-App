15cc7f91bf6cc89181ef05c6d03c71e5fc4d6262
ThiagoZanconi <thiagzanc@gmail.com>
2025-03-30 01:55:26 -0300

    Collections Update

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0efc3a40afa58e82e13a0b0e107ef47dea720a80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd6099dd4b6ac28a68b86b5931de16115b24952")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
