da1e8e57b2ec29181ac4eac3f73807c999ec144f
ThiagoZanconi <thiagzanc@gmail.com>
2025-04-03 19:05:49 -0300

    Verse Collection update

--- BlazorApp/Components/Services/BibleAPIService.cs
+++ BlazorApp/Components/Services/BibleAPIService.cs
@@ -5,6 +5,8 @@ namespace BlazorApp.Components.Services{
     public interface IBibleApiService
     {
         Task<List<Collection>?> GetCollectionsAsync(string token);
+        Task<List<Verse>?> GetVerseCollectionAsync(string token, string name);
+        Task PostVerseToCollectionAsync(string token, string name, Verse verse);
     }
     public class BibleApiService: IBibleApiService{
         private readonly HttpClient _httpClient;
@@ -12,6 +14,7 @@ namespace BlazorApp.Components.Services{
         {
             _httpClient = httpClient;
         }
+
         public async Task<List<Collection>?> GetCollectionsAsync(string token){
             var request = new HttpRequestMessage(HttpMethod.Get, "api/collection");
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
@@ -27,6 +30,39 @@ namespace BlazorApp.Components.Services{
                 Console.WriteLine("Error: "+e.Message);
             }
             return null;
+        }
+
+        public async Task<List<Verse>?> GetVerseCollectionAsync(string token, string name){
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/collection/"+name);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.SendAsync(request);
+            try{
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<List<Verse>>();  
+                }
+            }
+            catch(Exception e){
+                Console.WriteLine("Error: "+e.Message);
+            }
+            return null;
+        }
+
+        public async Task PostVerseToCollectionAsync(string token, string name, Verse verse){
+            var request = new HttpRequestMessage(HttpMethod.Post, "api/collection/"+name+"/"+verse.book_id+"/"+verse.chapter+":"+verse.verse);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.SendAsync(request);
+            try{
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Verso agregado a la coleccion exitosamente");
+                }
+            }
+            catch(Exception e){
+                Console.WriteLine("Error: "+e.Message);
+            }
         }
         
     }
